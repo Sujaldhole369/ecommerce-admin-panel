b56ba28a1a974a79730dcca1f329c846d84bb8ae
bigboss1999 <55378755+bigboss1999@users.noreply.github.com>
2025-04-23 16:37:09 +0530

    Changed the fetch url

--- src/Components/AddProduct/AddProduct.jsx
+++ src/Components/AddProduct/AddProduct.jsx
@@ -6,7 +6,6 @@ import { useState } from "react";
 import upload_area from "../../assets/upload_area.svg";
 const AddProduct = () => {
   const [image, setImage] = useState(false);
-  const backendURL = process.env.REACT_APP_BACKEND_URL;
   const [productDetails, setProductDetails] = useState({
     name: "",
     image: "",
@@ -30,7 +29,7 @@ const AddProduct = () => {
     let formData = new FormData();
     formData.append("product", image);
 
-    await fetch(`${backendURL}/upload`, {
+    await fetch(`https://backend-ecommerce-90ji.onrender.com/upload`, {
       method: "POST",
       headers: {
         Accept: "application/json",
@@ -45,7 +44,7 @@ const AddProduct = () => {
     if (responseData.success) {
       product.image = responseData.image_url;
       console.log(product);
-      await fetch(`${backendURL}/addproduct`,{
+      await fetch(`https://backend-ecommerce-90ji.onrender.com/addproduct`,{
         method: 'POST',
         headers: {
             Accept: 'application/json',
